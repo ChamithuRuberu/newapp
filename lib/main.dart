a8237d5fb61ee793e48fef5337c787c192c43df9
tcdruberu <94827905+tcdruberu@users.noreply.github.com>
2022-07-12 07:18:57 -0700

    update widgetdata

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
-void main()
-{
-  runApp(MyApp());
-}
+void main()=>runApp(MyApp());
 
-class MyApp extends StatelessWidget{
 
+
+class MyApp extends StatelessWidget {
+
+  var questionIndex=0;
+
+  void aq(){
+    questionIndex=questionIndex+1;
+    print(questionIndex);
+    }
 
   Widget build(BuildContext context)
   {
-    return MaterialApp(home:Text('hello'));
+    var questions=[
+      'what\'s your favourite color?'
+      'what\'s your favourite animal?'
+    ];
+    return MaterialApp(home:Scaffold(
+      appBar: AppBar(title:Text( 'Hello chami',)
+      ),
+      body:Column(
+        children: [
+          Text(
+            questions[questionIndex],
+          ),         // Text(questions[])
+          RaisedButton(child:Text('ansewer 1'),onPressed: aq,),
+          RaisedButton(child:Text('ansewer 2'),onPressed: () => print('answer 2 chosen ..'),),
+          RaisedButton(child:Text('ansewer 3'),onPressed:(){
+            print('answer 3 chosen..');
+          },),
+      ]
+      )
+    ));
   }
 }
